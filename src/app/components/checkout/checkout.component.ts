bee66626ae23277a4f9291b8be04815aaa6de67c
lilianwang <wanglijun00123172@gmail.com>
2023-01-17 21:18:55 -0500

    Add check box between Shipping and Billing Address

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -50,6 +50,14 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  copyShippingAddressToBillingAddress(event){
+    if(event.target.checked){
+      this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }else{
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+  }
+
   onSubmit() {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup!.get('customer')?.value);
