d841eabb4f70427f4281bb8c8fe9cd50978e0026
lilianwang <wanglijun00123172@gmail.com>
2023-01-22 21:30:21 -0500

    v1.3SessionStorage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,7 +11,24 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() {}
+  storage: Storage = sessionStorage;
+  // storage: Storage = localStorage;
+
+  constructor() {
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
 
   addToCart(theCartItem: CartItem) {
     // check if we already have the item in our cart
@@ -55,6 +72,9 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
   }
 
   // log cart data just for debugging purposes
