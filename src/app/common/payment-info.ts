73ac5791e96ce159b1adbf3fc945b2749b8639f8
lilianwang <wanglijun00123172@gmail.com>
2023-01-28 11:34:52 -0500

    minor code format change

--- src/app/common/payment-info.ts
+++ src/app/common/payment-info.ts
@@ -1,3 +1,7 @@
 export class PaymentInfo {
-  constructor(public amount?: number, public currency?: string, public receiptEmail?: string) {}
+  constructor(
+    public amount?: number,
+    public currency?: string,
+    public receiptEmail?: string
+  ) {}
 }
